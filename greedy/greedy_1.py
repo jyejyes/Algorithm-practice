a2ca9c430624df2717dd7a3a18bfa33fb524b935
brie <jh100m1@gmail.com>
2022-12-07 16:07:01 +0900

    :zap: greedy_1 코드 변경

--- greedy/greedy_1.py
+++ greedy/greedy_1.py
@@ -2,7 +2,7 @@ def mininum_coin(total_price):
     coin_num=0;
     for coin in [500,100,50,10]:
         coin_num+=total_price//coin
-        total_price-=((total_price//coin)*coin)
+        total_price%=coin # 더 간단한 방법으로 변경
     return coin_num;
     
 print(mininum_coin(int(input())))
